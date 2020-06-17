79321f0a38ad38908fff0391c97bc11057726aff
Your Name <you@example.com>
2020-06-17 23:03:22 +0900

    インデント修正

--- app/javascript/components/UserBox.js
+++ app/javascript/components/UserBox.js
@@ -4,66 +4,61 @@ import Sticky from "./Sticky"
 
 class UserBox extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-            var tasks = this.props.user.tasks ? this.props.user.tasks : {};
+    var tasks = this.props.user.tasks ? this.props.user.tasks : {};
 
-        this.state = { tasks: tasks };
+    this.state = { tasks: tasks };
 
-        this.onDrop = this.onDrop.bind(this);
+    this.onDrop = this.onDrop.bind(this);
     this.updateTaskList = this.updateTaskList.bind(this);
     this.preventDefault = this.preventDefault.bind(this);
 
-            this.props.dropHandlerRegister(this.props.user.id, this.updateTaskList);
-
+    this.props.dropHandlerRegister(this.props.user.id, this.updateTaskList);
   }
 
-      preventDefault(event) {
+  preventDefault(event) {
     event.preventDefault();
   }
 
-    onDrop(event) {
-        var dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
+  onDrop(event) {
+    var dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
 
-            this.props.onTaskDrop(dropData.now_user_id, this.props.user.id, dropData.task);
-
+    this.props.onTaskDrop(dropData.now_user_id, this.props.user.id, dropData.task);
   }
 
-          updateTaskList(prev_user_id, next_user_id, task) {
-        if (prev_user_id == next_user_id) {
+  updateTaskList(prev_user_id, next_user_id, task) {
+    if (prev_user_id == next_user_id) {
       return;
     }
 
-            if (prev_user_id == this.props.user.id) {
-            this.deleteTask(task.id);
+    if (prev_user_id == this.props.user.id) {
+      this.deleteTask(task.id);
     }
 
-            if (next_user_id == this.props.user.id) {
-            this.addTask(task);
+    if (next_user_id == this.props.user.id) {
+      this.addTask(task);
     } 
-
   }
 
-    deleteTask(task_id) {
+  deleteTask(task_id) {
     var tasks = this.state.tasks;
 
-                delete tasks[task_id];
+    delete tasks[task_id];
 
-        this.setState({tasks: tasks});
-
+    this.setState({tasks: tasks});
   }
 
-    addTask(task) {
+  addTask(task) {
     var tasks = this.state.tasks;
 
-        tasks[task.id] = task;
+    tasks[task.id] = task;
 
-        this.setState({tasks: tasks});
-
+    this.setState({tasks: tasks});
   }
 
-    render () {
+  render () {
     return (
       <React.Fragment>
         <div id={"user-" + this.props.user.id} className="UserBox" onDrop={this.onDrop} onDragOver={this.preventDefault} >
